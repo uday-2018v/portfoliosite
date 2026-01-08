6b4c67244898e0a61bf1501b6aa9e08b1f54ccd2
= <=>
2026-01-08 18:48:43 +0530

    Frontend build setup for Render

--- my-project/vite.config.js
+++ my-project/vite.config.js
@@ -5,4 +5,12 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+    build: {
+    outDir: 'dist'   // Ensure build folder name
+  },
+    server: {
+    host: '0.0.0.0',  // important! Render ko detect karne ke liye
+    port: process.env.PORT || 5173
+  }
+
 })
